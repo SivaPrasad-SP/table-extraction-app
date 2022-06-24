3609b4c4e07529aea7d78da342d1b7b3b5f3163f
csivaprasad <sivaprasad.c@lotusinsights.in>
2022-06-24 15:26:37 +0530

    torch-cpu

--- app/main.py
+++ app/main.py
@@ -1,18 +1,13 @@
 from flask import Flask, request, Response
-# import pytesseract
 import cv2 as cv
 import numpy as np
 import torch
 import requests
 import json
-# import pandas as pd
 
 app = Flask(__name__)
 
-# pytesseract.pytesseract.tesseract_cmd = "/app/.apt/usr/bin/tesseract"
-# pytesseract.pytesseract.tesseract_cmd = "Tesseract-OCR/tesseract.exe"
-
-model_name = 'yolo_model/best.pt' # last
+model_name = 'yolo_model/best.pt'
 model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_name, force_reload=True)
 
 @app.route("/", methods=['GET', 'POST'])
@@ -21,7 +16,7 @@ def home_view():
                 fs = request.files['file'] #.get('file')
                 frame = cv.imdecode(np.frombuffer(fs.read(), np.uint8), cv.IMREAD_UNCHANGED)
         else:
-                file_name = 'images/invo_7.jpg'
+                file_name = 'images/invo_7.png'
                 frame = cv.imread(file_name)
 
         tbl_img = frame.copy()
@@ -31,8 +26,7 @@ def home_view():
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
         ret, frame_thresh = cv.threshold(gray, 150, 255, cv.THRESH_BINARY)
 
-        h, w = frame_thresh.shape #[0], frame.shape[1]
-        # print(h, w, ' : ', frame_thresh.shape)
+        h, w = frame_thresh.shape
 
         device = 'cpu'
         model.to(device)
@@ -46,12 +40,8 @@ def home_view():
                 if labels[i] == 1:
                         row = cord[i]
                         x1, y1, x2, y2 = int(row[0]*w), int(row[1]*h), int(row[2]*w), int(row[3]*h)
-                        # print(x1, y1, x2, y2)
                         arr.append([y1, y2, x1, x2])
-                #cv.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
         nt = len(arr)
-
-        # img[y1:y2, x1:x2]
 
         areas = []
         for i in arr:
@@ -77,16 +67,12 @@ def home_view():
 
                 tbl_rows = this_response["data"]["img2tbl"]
 
-                # result_df = pd.DataFrame(tbl_rows[1:], columns=tbl_rows[0])
-                # result_df.to_csv('outputs/result.csv', index=False)
                 with open("outputs/result.csv", "w") as f:
                         for row in tbl_rows:
                                 f.write("%s\n" % ','.join(str(col) for col in row))
         else:
                 print(resp.content)
-        # print(resp.content, '<<<<<<-----------------------')
-
-        # 'img2txt': pytesseract.image_to_string(tbl_img),
+        
         response_data = {'data': {'no_of_tables ': nt, 'img_shape':img.shape, 'tbl_shape': tbl_img.shape, 'table_data': tbl_rows}}
         return response_data
 
